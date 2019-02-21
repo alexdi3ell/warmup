2f41911702b76712c1027d28ce63200f7900d147
alexdi3ell <a.n.degilevich@gmail.com>
2019-02-21 22:42:34 +0300

    Commit warmup

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function warmup(temperature) {
   // your implementation here
+  let rCels = +temperature;
+  let rFar = 0;
+  rFar = Math.round(9/5*rCels + 32); 
+  return rFar;
 };
